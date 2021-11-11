5716c960101e5eb0728f2b4e70debb2a98c01cea
ryguy4125 <ryan.a.revilla@gmail.com>
2021-11-10 22:10:19 -0800

    inital commit

---
+++ nuxt.config.js
@@ -0,0 +1,61 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'oal-example-headless',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+      {
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com/",
+        crossorigin: "anonymous",
+      },
+      {
+        rel: "preload",
+        as: "style",
+        href:
+          "https://fonts.googleapis.com/css?family=Raleway:300,400,400i,500,600,700|Roboto:300,300i,400,400i,500,700&display=swap",
+        crossorigin: "anonymous",
+      },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css?family=Raleway:300,400,400i,500,600,700|Roboto:300,300i,400,400i,500,700&display=swap",
+        media: "print",
+        onload: "this.media='all'",
+      },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    transpile: [/^@storefront-ui/],
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+<div id="app">
+  <h1>Hello!!!</h1>
+  <SfButton class="button-center">Add to Cart</SfButton>
+  <SfBadge class="color-primary">Limited Offer</SfBadge>
+</div>
+</template>
+
+<script>
+import {SfButton, SfBadge } from "@storefront-ui/vue";
+import "@storefront-ui/vue/styles.scss";
+export default {
+  name: "Home",
+  components: {
+    SfButton,
+    SfBadge
+    }
+}
+</script>
+
+<style lang="scss">
